b3b699a2be9151fa6dffc7000fa13b9c5758f97a
Rodolfo <rodolfodossantos29@gmail.com>
2019-12-21 17:56:28 -0300

    Atualização para vUEJS

---
+++ js/vue_certificados.js
@@ -0,0 +1,22 @@
+var qualificacoes = new Vue({
+  el: "#qualificacoes",
+  data: {
+    desenvolvimento: [],
+    ferramentas: []
+  },
+
+  methods: {
+    fetchQualificacoes() {
+      fetch("../api/qualificacoes.json")
+        .then(r => r.json())
+        .then(qualificacoes => {
+          this.desenvolvimento = qualificacoes.desenvolvimento
+          this.ferramentas = qualificacoes.ferramentas
+        })
+    }
+  },
+
+  created() {
+    this.fetchQualificacoes();
+  }
+})
