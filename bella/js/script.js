094c93ecdf173a7207c7eadc15fe2db3be4d5751
Rodolfo <rodolfodossantos29@gmail.com>
2020-03-08 16:28:56 -0300

    Site da Bella

---
+++ bella/js/script.js
@@ -0,0 +1,10 @@
+  /* DOM is ready
+  ------------------------------------------------*/
+  $(function(){
+
+    if(renderPage) {
+      $('body').addClass('loaded');
+    }
+
+    $('.tm-current-year').text(new Date().getFullYear());  // Update year in copyright
+  });
